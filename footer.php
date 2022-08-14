ef912db406b9324a196640bab4e26158ca02e3ec
Melvin Lenglet <melvin19690@gmail.com>
2022-08-15 01:11:36 +0200

    Ajout de l'en-tête et du bas de page

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+ <hr class="balise2" />
+ <span class="vertical-line"></span> 
+ <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Mentions légales</a>
+ <span class="vertical-line"></span>
+ <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Contact</a>
+ <span class="vertical-line"></span>  
+</footer>
