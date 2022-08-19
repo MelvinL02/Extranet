87e3859b8c993ad5464070d323275255818adbac
Melvin Lenglet <melvin19690@gmail.com>
2022-08-19 22:07:20 +0200

    Mise à jour des formulaires de mise à jour utilisateur

--- user_form.php
+++ user_form.php
@@ -20,93 +20,102 @@
 ?>
 
 <!DOCTYPE html>
-<html>
-  <head>
-   <meta charset="utf-8" />
-   <link rel="stylesheet" href="style.css" />
-   <title>Le-Groupement-Banque-Assurance-Français</title>
-  </head>
- 
-<?php include_once('header.php'); ?> 
-  
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Le-Groupement-Banque-Assurance-Français</title>
+    </head>
+    
   <body>
-   <div class="sucess">
-   <h2><?php echo $bienvenue?></h2>
-   [ <a href="session.php">Accueil</a> ]
-   <br>[ <a href="deconnexion.php">Se déconnecter</a> ]
-   </div>
+        <header id="header">
+      <a href="session.php"><img id="logo" src="images/gbaf.png" alt="Logo du GBAF" title="Le Groupement Banque-Assurance Français" /></a> 
+      <div id="user">
+         <div class="userImg"><img id="account" src="images/icone_account.png" /></div>
+         <div id="userLink">   
+           <p><a href="user_form.php"><?php echo htmlspecialchars($bienvenue);?></a></p>
+           <p id="deco"><a href="deconnexion.php">Se déconnecter</a></p>
+         </div>
+      </div>
+        </header>
+    
+    <div id="containeur">
+    <h1><br><?php echo $bienvenue?></h1>
 
-  <div id="account">
-  <h1><?php echo $bienvenue?></h1>
-
- <section class="form">
-        <h2><u>Changer mon nom d'utilisateur</u></h2>
+    <section id="form">
         <form action="user_update.php"  method="post" novalidate>
-        <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>" />
-            <p><b><label for="username">Nom d'utilisateur : </label><br />
-            <input type="text" name="username" id="username" value="<?php echo $username?>" /></p></b>
+        <h2><u>Changer mon nom d'utilisateur</u></h2>
+        <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>"/>
+        <br><p><label for="username">Nom d'utilisateur : </label><br />
+        <br> <input type="text" name="username" id="username" value="<?php echo $username?>" /></p></b>
             <p class="error"></p>
             <p class="confirm"></p>
-            <input type="submit" name="send" value="Valider">
+        <br><input type="submit" name="send" value="Valider">
         </form>
     </section>
 
-    <section class="form">
+    <section id="form">
+        <form action="user_update.php" method="post" novalidate>
         <h2><u>Changer mon mot de passe</u></h2>
-        <form action="user_update.php" method="post" novalidate>
         <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>" />
-            <p><b><label for="password">Mon nouveau mot de passe : </label><br />
-            <input type="password" name="password" id="password" required /></p></b>
+        <br><p><label for="password">Mon nouveau mot de passe : </label><br />
+        <br><input type="password" name="password" id="password" required /></p></b>
             <p class="error"></p>
-            <p><b><label for="repassword">Confirmation du nouveau mot de passe : </label><br />
-            <input type="password" name="repassword" id="repassword" required /></p></b>
+            <br><p><label for="repassword">Confirmation du nouveau mot de passe : </label><br />
+            <br><input type="password" name="repassword" id="repassword" required /></p></b>
             <p class="error"></p>
             <p class="confirm"></p>
-            <input type="submit" name="send_1" value="Valider">
+        <br><input type="submit" name="send_1" value="Valider">
         </form>
     </section>
 
-    <section class="form">
+    <section id="form">
+        <form action="user_update.php" method="post" novalidate>
         <h2><u>Changer mon nom</u></h2>
-        <form action="user_update.php" method="post" novalidate>
         <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>" />
-            <p><b><label for="nom">Nom : </label><br />
-            <input type="text" name="nom" id="nom" value="<?php echo $nom?>"  /></p></b>
+        <br><p><label for="nom">Nom : </label><br />
+        <br><input type="text" name="nom" id="nom" value="<?php echo $nom?>"  /></p></b>
             <p class="error"></p>
             <p class="confirm"></p>
-            <input type="submit" name="send_2" value="Valider">
+        <br><input type="submit" name="send_2" value="Valider">
         </form>
     </section>
 
-    <section class="form">
+    <section id="form">
+        <form action="user_update.php" method="post" novalidate>
         <h2><u>Changer mon prénom</u></h2>
-        <form action="user_update.php" method="post" novalidate>
         <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>" />
-            <p><b><label for="prenom">Prénom : </label><br />
-            <input type="text" name="prenom" id="prenom" value="<?php echo $prenom?>"  /></p></b>
+        <br><p><label for="prenom">Prénom : </label><br />
+        <br><input type="text" name="prenom" id="prenom" value="<?php echo $prenom?>"  /></p></b>
             <p class="error"></p>
             <p class="confirm"></p>
-            <input type="submit" name="send_3" value="Valider">
+        <br><input type="submit" name="send_3" value="Valider">
         </form>
     </section>
 
-    <section class="form">
+    <section id="form">
+        <form action="user_update.php" method="post" novalidate>
         <h2><u>Changer ma question secrète et ma réponse</u></h2>
-        <form action="user_update.php" method="post" novalidate>
         <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>" />
-            <p><b><label for="question">Ma nouvelle question secrète : </label><br />
-            <input type="text" name="question" id="question" value="" required /></p></b>
+        <br><p><label for="question">Ma nouvelle question secrète : </label><br />
+        <br><input type="text" name="question" id="question" value="" required /></p></b>
             <p class="error"></p>
-            <p><b><label for="reponse">Ma nouvelle réponse secrète : </label><br />
-            <input type="text" name="reponse" id="reponse" value="" required /></p></b>
+        <br><p><label for="reponse">Ma nouvelle réponse secrète : </label><br />
+        <br><input type="text" name="reponse" id="reponse" value="" required /></p></b>
             <p class="error"></p>
             <p class="confirm"></p>
-            <input type="submit" name="send_4" value="Valider">
+        <br><input type="submit" name="send_4" value="Valider">
         </form>
     </section>
- </div>
+  </div>
 
-<?php include_once('footer.php'); ?>
-
- </body>
+    <footer>
+       <hr class="balise2" />
+       <span class="vertical-line"></span> 
+       <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Mentions légales</a>
+       <span class="vertical-line"></span>
+       <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Contact</a>
+       <span class="vertical-line"></span>  
+    </footer>
+  </body>
 </html>
