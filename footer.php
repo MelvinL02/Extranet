21966d92b3f093da1c093a3562fed266487ee825
MelvinL02 <109614651+MelvinL02@users.noreply.github.com>
2022-09-04 07:23:37 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
- <hr class="balise2" />
- <span class="vertical-line"></span> 
- <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Mentions légales</a>
- <span class="vertical-line"></span>
- <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Contact</a>
- <span class="vertical-line"></span>  
-</footer>
