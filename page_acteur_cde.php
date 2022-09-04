929adc0ba924e449000ab16af1381fbffa14de81
Melvin Lenglet <melvin19690@gmail.com>
2022-09-04 07:17:21 +0200

    Ajout des pages acteurs manquantes

--- page_acteur_cde.php
+++ page_acteur_cde.php
@@ -3,11 +3,11 @@
    include("variables.php");
    include("connexion.php");
 
-   $acteur = 1;
-   $sql = ("SELECT * from comments WHERE actor_id = 1 ORDER BY created_at DESC");
+   $acteur = 4;
+   $sql = ("SELECT * from comments WHERE actor_id = 4 ORDER BY created_at DESC");
    $statement = $pdo->query($sql);
    $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
-
+   
    $bdd = new PDO("mysql:host=localhost;dbname=extranet_gbaf;charset=utf8", "root", "root");
    if(isset($_GET['id']) AND !empty($_GET['id'])) {
       $get_id = htmlspecialchars($_GET['id']);
@@ -54,22 +54,13 @@
    <main>
     <section id="presentation_acteur">
         <figure class="logo_page_acteur">
-            <img src="images/formation_co.png" alt="logo de l'acteur"/>
+            <img src="images/CDE.png" alt="logo de l'acteur"/>
         </figure>
         <figcaption hidden>Logo de Formation&co</figcaption>
         <div class="description_page_acteur">
-            <h2>Formation&co</h2>
-            <p><p>Formation&co est une association française présente sur tout le territoire.</p>
-            <p>Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.</p>
-            <p>Notre proposition :</p> 
-            <ul>
-            <li><span  class="li_content">un financement jusqu’à 30 000€ ;</span></li>
-            <li><span  class="li_content">un suivi personnalisé et gratuit ;</span></li>
-            <li><span  class="li_content">une lutte acharnée contre les freins sociétaux et les stéréotypes.</span></li>
-            </ul></p>
-            
-            <p>Le financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . Nous collaborons avec des personnes talentueuses et motivées.</p>
-            <p>Vous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! Nos financements s’adressent à tous.</p></p>
+        <h2>CDE</h2>
+            <p><p>La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. </p>
+            <p>Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.</p></p>
         </div>
         <div class="like">
             <span class="fas fa-thumbs-up fa-lg"><a href="action.php?t=1&id=<?= $id ?>" style="color:green;"> J'aime (<?= $likes ?>)</a></span>
@@ -78,7 +69,7 @@
     </section>
     
     <section id="form">
-     <form name="fo" action="commentaires.php" method="post">
+     <form action="commentaires.php" method="post">
       <h2><u>Mon commentaire :</u></h2>
       <input type="hidden" name="actor_id" value="<?php echo htmlspecialchars($acteur)?>"/>
       <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>"/>
