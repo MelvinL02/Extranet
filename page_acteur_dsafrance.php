a1bdb12853f83e10b54c93360f20db83ce1f4b8a
Melvin Lenglet <melvin19690@gmail.com>
2022-09-08 19:21:35 +0200

    Mise à jour des pages acteurs avec message like/dislike

--- page_acteur_dsafrance.php
+++ page_acteur_dsafrance.php
@@ -3,23 +3,26 @@
    include("variables.php");
    include("connexion.php");
 
+   // Affichage des commentaires de la page
+
    $acteur = 3;
-   $sql = ("SELECT * from comments WHERE actor_id = 3 ORDER BY created_at DESC");
+   $sql = ("SELECT * from comments WHERE id_actor = 3 ORDER BY created_at DESC");
    $statement = $pdo->query($sql);
    $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-   $bdd = new PDO("mysql:host=localhost;dbname=extranet_gbaf;charset=utf8", "root", "root");
+   // Affichage des nombres de like de l'acteur
+
    if(isset($_GET['id']) AND !empty($_GET['id'])) {
       $get_id = htmlspecialchars($_GET['id']);
-      $article = $bdd->prepare('SELECT * FROM users WHERE id = ?');
+      $article = $pdo->prepare('SELECT * FROM users WHERE id_user = ?');
       $article->execute(array($get_id));
       if($article->rowCount() == 1) {
          $article = $article->fetch();
-         $id = $article['id'];
-         $likes = $bdd->prepare('SELECT id FROM likes WHERE id_article = ?');
+         $id = $article['id_user'];
+         $likes = $pdo->prepare('SELECT id FROM likes WHERE id_article = ?');
          $likes->execute(array($id));
          $likes = $likes->rowCount();
-         $dislikes = $bdd->prepare('SELECT id FROM dislikes WHERE id_article = ?');
+         $dislikes = $pdo->prepare('SELECT id FROM dislikes WHERE id_article = ?');
          $dislikes->execute(array($id));
          $dislikes = $dislikes->rowCount();
       } else {
@@ -28,24 +31,45 @@
    } else {
       die('Erreur');
    }
+
+   // Vérifier si l'utilisateur a liké/disliké l'acteur
+
+   if($_SESSION["id_user"])
+      $select = $pdo->prepare ("SELECT u.id_user FROM users u INNER JOIN likes l ON u.id_user = l.id_membre WHERE l.id_article = 3");
+      $select->execute(array($_SESSION["id_user"]));
+    if($select->rowCount() > 0) {
+      $select = $select->fetch();
+      $message="Vous avez aimé cet acteur.";
+    } else {
+      $select = $pdo->prepare ("SELECT u.id_user FROM users u INNER JOIN dislikes d ON u.id_user = d.id_membre WHERE d.id_article = 3");
+      $select->execute(array($_SESSION["id_user"]));
+    if($select->rowCount() > 0) {
+      $select = $select->fetch();
+      $message="Vous n'avez pas aimé cet acteur.";
+    } else {
+      $message="";
+    }
+   }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="styles.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
-        <title>Le-Groupement-Banque-Assurance-Français</title>
-    </head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="icon" href="images/fav_icon_gbaf.png">
+    <title>Le Groupement Banque Assurance Français</title>
+  </head>
     
   <body>
         <header id="header">
-      <a href="session.php"><img id="logo" src="images/gbaf.png" alt="Logo du GBAF" title="Le Groupement Banque-Assurance Français" /></a> 
+      <a href="session.php"><img id="logo" src="images/gbaf.png" alt="Logo du GBAF" title="Le Groupement Banque-Assurance Français"></a> 
       <div id="user">
-         <div class="userImg"><img id="account" src="images/icone_account.png" /></div>
+         <div class="userImg"><img id="account" alt="Icone de l'utilisateur" src="images/icone_account.png"></div>
          <div id="userLink">   
-           <p><a href="user_form.php"><?php echo htmlspecialchars($bienvenue);?></a></p>
+           <p><a href="user_form.php"><?php echo htmlspecialchars($_SESSION["prenomNom"])?></a></p>
            <p id="deco"><a href="deconnexion.php">Se déconnecter</a></p>
          </div>
       </div>
@@ -56,26 +80,25 @@
         <figure class="logo_page_acteur">
             <img src="images/Dsa_france.png" alt="logo de l'acteur"/>
         </figure>
-        <figcaption hidden>Logo de DSA France</figcaption>
         <div class="description_page_acteur">
         <h2>DSA France</h2>
-            <p><p>Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.</p>
+            <p>Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.</p>
             <p>Nous accompagnons les entreprises dans les étapes clés de leur évolution.</p>
             <p>Notre philosophie : s’adapter à chaque entreprise.</p>
             <p>Nous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises.</p>
-            </p>
         </div>
         <div class="like">
             <span class="fas fa-thumbs-up fa-lg"><a href="action.php?t=1&id=<?= $id ?>" style="color:green;"> J'aime (<?= $likes ?>)</a></span>
             <span class="fas fa-thumbs-down fa-lg"><a href="action.php?t=2&id=<?= $id ?>" style="color:red;"> Je n'aime pas (<?= $dislikes ?>)</a></span>
         </div>
+        <div class="erreur">
+            <?php echo $message ?></div>
     </section>
     
     <section id="form">
      <form action="commentaires.php" method="post">
       <h2><u>Mon commentaire :</u></h2>
-      <input type="hidden" name="actor_id" value="<?php echo htmlspecialchars($acteur)?>"/>
-      <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>"/>
+      <input type="hidden" name="id_actor" value="<?php echo htmlspecialchars($acteur)?>"/>
       <input type="hidden" name="prenom" value="<?php echo htmlspecialchars($_SESSION["prenom"])?>"/>
       <p><textarea name="comment" rows="8" cols="90" placeholder="Votre commentaire"></textarea></p>
       <input type="submit" name="valider" value="Envoyer">
@@ -88,23 +111,22 @@
     <div class="commentaires">
         <div class="head_comment">
             <div>
-                <p class="firstname"><b><?= $comment['prenom']?></b></p>
-                <p class="date_add">Posté le <?= $comment['created_at'] ?></p>
+                <p class="firstname"><b><?php echo htmlspecialchars($comment['prenom'])?></b></p>
+                <p class="date_add">Posté le <?php echo htmlspecialchars($comment['created_at'])?></p>
               </div>
         </div>
-        <p class="comment"><?= $comment['comment']; ?></p>
+        <p class="comment"><?php echo htmlspecialchars($comment['comment'])?></p>
     </div>
     <?php endforeach; ?>
     </section>
    </main>
 
-     <footer>
-        <hr class="baliseFooter" />
+   <footer>
         <span class="vertical-line"></span> 
-        <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Mentions légales</a>
+        <a href="#" style="color:white" >Mentions légales</a>
         <span class="vertical-line"></span>
-        <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Contact</a>
-        <span class="vertical-line"></span>  
-     </footer>
+        <a href="#" style="color:white" >Contact</a>
+        <span class="vertical-line"></span>
+   </footer>
   </body>
 </html>
