6fe7a65f593d7c0f5603de778976885666e50156
Melvin Lenglet <melvin19690@gmail.com>
2022-09-08 19:15:39 +0200

    Mise à jour du système de commentaires et ajout d'un message pour les likes

--- action.php
+++ action.php
@@ -1,44 +1,48 @@
 <?php
    session_start();
+   include("connexion.php");
    include("variables.php");
 
-   $bdd = new PDO("mysql:host=localhost;dbname=extranet_gbaf;charset=utf8", "root", "root");
+   // Insertion et suppression des likes
+
    if(isset($_GET['t'],$_GET['id']) AND !empty($_GET['t']) AND !empty($_GET['id'])) {
    $getid = (int) $_GET['id'];
    $gett = (int) $_GET['t'];
-   $sessionid = $_SESSION["id"];
-   $check = $bdd->prepare('SELECT id FROM users WHERE id = ?');
+   $sessionid = $_SESSION["id_user"];
+   $check = $pdo->prepare('SELECT id_user FROM users WHERE id_user = ?');
    $check->execute(array($getid));
    if($check->rowCount() == 1) {
       if($gett == 1) {
-         $check_like = $bdd->prepare('SELECT id FROM likes WHERE id_article = ? AND id_membre = ?');
+         $check_like = $pdo->prepare('SELECT id FROM likes WHERE id_article = ? AND id_membre = ?');
          $check_like->execute(array($getid,$sessionid));
-         $del = $bdd->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
+         $del = $pdo->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
          $del->execute(array($getid,$sessionid));
          if($check_like->rowCount() == 1) {
-            $del = $bdd->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
+            $del = $pdo->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
             $del->execute(array($getid,$sessionid));
          } else {
-            $ins = $bdd->prepare('INSERT INTO likes (id_article, id_membre) VALUES (?, ?)');
+            $ins = $pdo->prepare('INSERT INTO likes (id_article, id_membre) VALUES (?, ?)');
             $ins->execute(array($getid, $sessionid));
          }
+
+   // Insertion et suppression des dislikes
          
       } elseif($gett == 2) {
-         $check_like = $bdd->prepare('SELECT id FROM dislikes WHERE id_article = ? AND id_membre = ?');
+         $check_like = $pdo->prepare('SELECT id FROM dislikes WHERE id_article = ? AND id_membre = ?');
          $check_like->execute(array($getid,$sessionid));
-         $del = $bdd->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
+         $del = $pdo->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
          $del->execute(array($getid,$sessionid));
          if($check_like->rowCount() == 1) {
-            $del = $bdd->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
+            $del = $pdo->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
             $del->execute(array($getid,$sessionid));
          } else {
-            $ins = $bdd->prepare('INSERT INTO dislikes (id_article, id_membre) VALUES (?, ?)');
+            $ins = $pdo->prepare('INSERT INTO dislikes (id_article, id_membre) VALUES (?, ?)');
             $ins->execute(array($getid, $sessionid));
          }
       }
       header("Location: $referer");
    } else {
-      exit ('Erreur fatale.');;
+      exit ('Erreur fatale.');
    }
 } else {
    exit ('Erreur fatale.');
--- commentaires.php
+++ commentaires.php
@@ -1,13 +1,15 @@
 <?php
    session_start();
    include("variables.php");
+
+   // Insertion des commentaires dans la page acteur concernée
  
-  if($_SESSION["id"])
-   $actor = $_POST["actor_id"];
+  if($_SESSION["id_user"])
+   $actor = $_POST["id_actor"];
    $prenom = $_POST["prenom"];
    $comment = $_POST["comment"];
    $valider = $_POST["valider"];
-   $message="";
+   
 
   if(isset($valider)){
     try {
@@ -17,100 +19,33 @@
         die('Erreur : '.$e->getMessage());
       }
 
-  if(empty($comment)) $message="Veuillez remplir le champ commentaire !";
+  if(empty($comment)) header("Location: $referer");
    else{
    $select = $pdo->prepare('SELECT prenom FROM comments WHERE prenom = ?');
    $select->execute([$prenom]);
   
-  if ($select->rowCount() > 6) {
+  if ($select->rowCount() > 8) {
 
-      $message = 'Vous avez déjà commenté !'; 
+      $erreur = "Vous avez trop commenté !"; 
    
   }else{
 
-    $ins = $pdo->prepare("INSERT INTO comments (actor_id,id,prenom,comment) VALUES (?,?,?,?)");
-    $executeIsOk = $ins->execute(array($actor, $_SESSION["id"], $prenom, $comment));
+    $ins = $pdo->prepare("INSERT INTO comments (id_actor,prenom,comment) VALUES (?,?,?)");
+    $executeIsOk = $ins->execute(array($actor, $prenom, $comment));
     }
 
   if($executeIsOk){
     
-    $message = 'Commentaire envoyé !'; 
+    $erreur = "Commentaire envoyé !"; 
+    header("Location: $referer");
 
- 
   }else{
 
-    $message = "Echec de l'envoi";
+    $erreur = "Echec de l'envoi";
+    header("Location: $referer");
     }
    }   
   }
-
-  $acteur = 1;
-  $sql = ("SELECT * from comments WHERE actor_id = 1 ORDER BY created_at DESC");
-  $statement = $pdo->query($sql);
-  $comments = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-   <head>
-       <meta charset="utf-8" />
-       <link rel="stylesheet" href="styles.css" />
-       <title>Le-Groupement-Banque-Assurance-Français</title>
-   </head>
-   
- <body>
-       <header id="header">
-     <a href="session.php"><img id="logo" src="images/gbaf.png" alt="Logo du GBAF" title="Le Groupement Banque-Assurance Français" /></a> 
-     <div id="user">
-        <div class="userImg"><img id="account" src="images/icone_account.png" /></div>
-        <div id="userLink">   
-          <p><a href="user_form.php"><?php echo htmlspecialchars($bienvenue);?></a></p>
-          <p id="deco"><a href="deconnexion.php">Se déconnecter</a></p>
-        </div>
-     </div>
-       </header>
-       
-  <main>
-   <section id="form">
-    <form name="fo" action="commentaires.php" method="post">
-     <h2><u>Mon commentaire :</u></h2>
-     <input type="hidden" name="actor_id" value="<?php echo htmlspecialchars($acteur)?>"/>
-     <input type="hidden" name="id" value="<?php echo htmlspecialchars($_SESSION["id"])?>"/>
-     <input type="hidden" name="prenom" value="<?php echo htmlspecialchars($_SESSION["prenom"])?>"/>
-     <p><textarea name="comment" rows="8" cols="150" placeholder="Votre commentaire"></textarea></p>
-     <input type="submit" name="valider" value="Envoyer">
-     <div class="erreur">
-     <?php echo $message ?></div>
-    </form>
-   </section>
-
-  <section id="sectionComment">
-   <h2>Commentaires</h2>
-   <?php foreach ($comments as $comment) : ?>
-   <div class="commentaires">
-       <div class="head_comment">
-           <div>
-               <p class="firstname"><b><?= $comment['prenom']?></b></p>
-               <p class="date_add">Posté le <?= $comment['created_at'] ?></p>
-             </div>
-       </div>
-       <p class="comment"><?= $comment['comment']; ?></p>
-   </div>
-   <?php endforeach; ?>
-   </section>
-  </main>
-
-    <footer>
-       <hr class="baliseFooter" />
-       <span class="vertical-line"></span> 
-       <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Mentions légales</a>
-       <span class="vertical-line"></span>
-       <a href="https://openclassrooms.com" title="Vous ne le regretterez pas !" style="color:white" >Contact</a>
-       <span class="vertical-line"></span>  
-    </footer>
- </body>
-</html>
-
-
 
 
