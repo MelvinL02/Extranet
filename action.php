85e0d90780b976cccc890ce150b9dddece7c4a26
Melvin Lenglet <melvin19690@gmail.com>
2022-09-13 00:02:49 +0200

    Indentation et commentaire sur les pages

--- action.php
+++ action.php
@@ -1,49 +1,68 @@
 <?php
-   session_start();
-   include("connexion.php");
-   include("variables.php");
+session_start();
+include "connexion.php";
+include "variables.php";
 
-   // Insertion et suppression des likes
+// Insertion et suppression des likes
 
-   if(isset($_GET['t'],$_GET['id']) AND !empty($_GET['t']) AND !empty($_GET['id'])) {
-   $getid = (int) $_GET['id'];
-   $gett = (int) $_GET['t'];
-   $sessionid = $_SESSION["id_user"];
-   $check = $pdo->prepare('SELECT id_user FROM users WHERE id_user = ?');
-   $check->execute(array($getid));
-   if($check->rowCount() == 1) {
-      if($gett == 1) {
-         $check_like = $pdo->prepare('SELECT id FROM likes WHERE id_article = ? AND id_membre = ?');
-         $check_like->execute(array($getid,$sessionid));
-         $del = $pdo->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
-         $del->execute(array($getid,$sessionid));
-         if($check_like->rowCount() == 1) {
-            $del = $pdo->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
-            $del->execute(array($getid,$sessionid));
-         } else {
-            $ins = $pdo->prepare('INSERT INTO likes (id_article, id_membre) VALUES (?, ?)');
-            $ins->execute(array($getid, $sessionid));
-         }
+if (
+    isset($_GET["t"], $_GET["id"]) and
+    !empty($_GET["t"]) and
+    !empty($_GET["id"])
+) {
+    $getid = (int) $_GET["id"];
+    $gett = (int) $_GET["t"];
+    $sessionid = $_SESSION["id_user"];
+    $check = $pdo->prepare("SELECT id_user FROM users WHERE id_user = ?");
+    $check->execute([$getid]);
+    if ($check->rowCount() == 1) {
+        if ($gett == 1) {
+            $check_like = $pdo->prepare(
+                "SELECT id FROM likes WHERE id_article = ? AND id_membre = ?"
+            );
+            $check_like->execute([$getid, $sessionid]);
+            $del = $pdo->prepare(
+                "DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?"
+            );
+            $del->execute([$getid, $sessionid]);
+            if ($check_like->rowCount() == 1) {
+                $del = $pdo->prepare(
+                    "DELETE FROM likes WHERE id_article = ? AND id_membre = ?"
+                );
+                $del->execute([$getid, $sessionid]);
+            } else {
+                $ins = $pdo->prepare(
+                    "INSERT INTO likes (id_article, id_membre) VALUES (?, ?)"
+                );
+                $ins->execute([$getid, $sessionid]);
+            }
 
-   // Insertion et suppression des dislikes
-         
-      } elseif($gett == 2) {
-         $check_like = $pdo->prepare('SELECT id FROM dislikes WHERE id_article = ? AND id_membre = ?');
-         $check_like->execute(array($getid,$sessionid));
-         $del = $pdo->prepare('DELETE FROM likes WHERE id_article = ? AND id_membre = ?');
-         $del->execute(array($getid,$sessionid));
-         if($check_like->rowCount() == 1) {
-            $del = $pdo->prepare('DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?');
-            $del->execute(array($getid,$sessionid));
-         } else {
-            $ins = $pdo->prepare('INSERT INTO dislikes (id_article, id_membre) VALUES (?, ?)');
-            $ins->execute(array($getid, $sessionid));
-         }
-      }
-      header("Location: $referer");
-   } else {
-      exit ('Erreur fatale.');
-   }
+            // Insertion et suppression des dislikes
+        } elseif ($gett == 2) {
+            $check_like = $pdo->prepare(
+                "SELECT id FROM dislikes WHERE id_article = ? AND id_membre = ?"
+            );
+            $check_like->execute([$getid, $sessionid]);
+            $del = $pdo->prepare(
+                "DELETE FROM likes WHERE id_article = ? AND id_membre = ?"
+            );
+            $del->execute([$getid, $sessionid]);
+            if ($check_like->rowCount() == 1) {
+                $del = $pdo->prepare(
+                    "DELETE FROM dislikes WHERE id_article = ? AND id_membre = ?"
+                );
+                $del->execute([$getid, $sessionid]);
+            } else {
+                $ins = $pdo->prepare(
+                    "INSERT INTO dislikes (id_article, id_membre) VALUES (?, ?)"
+                );
+                $ins->execute([$getid, $sessionid]);
+            }
+        }
+        header("Location: $referer");
+    } else {
+        exit("Erreur fatale.");
+    }
 } else {
-   exit ('Erreur fatale.');
+    exit("Erreur fatale.");
 }
--- commentaires.php
+++ commentaires.php
@@ -1,51 +1,47 @@
 <?php
-   session_start();
-   include("variables.php");
+session_start();
+include "variables.php";
 
-   // Insertion des commentaires dans la page acteur concernée
- 
-  if($_SESSION["id_user"])
-   $actor = $_POST["id_actor"];
-   $prenom = $_POST["prenom"];
-   $comment = $_POST["comment"];
-   $valider = $_POST["valider"];
-   
+// Insertion des commentaires dans la page acteur concernée
 
-  if(isset($valider)){
+if ($_SESSION["id_user"]) {
+    $actor = $_POST["id_actor"];
+}
+$prenom = $_POST["prenom"];
+$comment = $_POST["comment"];
+$valider = $_POST["valider"];
+
+if (isset($valider)) {
     try {
-        include("connexion.php");
-      } catch(Exception $e){
-    
-        die('Erreur : '.$e->getMessage());
-      }
-
-  if(empty($comment)) header("Location: $referer");
-   else{
-   $select = $pdo->prepare('SELECT prenom FROM comments WHERE prenom = ?');
-   $select->execute([$prenom]);
-  
-  if ($select->rowCount() > 8) {
-
-      $erreur = "Vous avez trop commenté !"; 
-   
-  }else{
-
-    $ins = $pdo->prepare("INSERT INTO comments (id_actor,prenom,comment) VALUES (?,?,?)");
-    $executeIsOk = $ins->execute(array($actor, $prenom, $comment));
+        include "connexion.php";
+    } catch (Exception $e) {
+        die("Erreur : " . $e->getMessage());
     }
 
-  if($executeIsOk){
-    
-    $erreur = "Commentaire envoyé !"; 
-    header("Location: $referer");
+    if (empty($comment)) {
+        header("Location: $referer");
+    } else {
+        $select = $pdo->prepare("SELECT prenom FROM comments WHERE prenom = ?");
+        $select->execute($prenom);
 
-  }else{
+        if ($select->rowCount() > 8) {
+            $erreur = "Vous avez trop commenté !";
+        } else {
+            $ins = $pdo->prepare(
+                "INSERT INTO comments (id_actor,prenom,comment) VALUES (?,?,?)"
+            );
+            $executeIsOk = $ins->execute([$actor, $prenom, $comment]);
+        }
 
-    $erreur = "Echec de l'envoi";
-    header("Location: $referer");
+        if ($executeIsOk) {
+            $erreur = "Commentaire envoyé !";
+            header("Location: $referer");
+        } else {
+            $erreur = "Echec de l'envoi";
+            header("Location: $referer");
+        }
     }
-   }   
-  }
+}
 ?>
 
 
